bec2c754037bd620ab99339acd97f47590948b1e
Derek Carvajal <d.carvajal@correo.unimet.edu.ve>
2024-09-18 12:09:26 -0400

    prueba github

---
+++ arepa.py
@@ -0,0 +1,12 @@
+agua = int(input("Cantidad de tazas de agua: "))
+harina = int(input("Cantidad de tazas de harina: "))
+sal = float(input("Cantidad de sal: "))//16
+aceite = float(input("Cantidad de aceite: "))//16
+print("El agua  es: ", agua)
+print("La harina es: ", harina)
+print("La sal es: ", sal)
+print("El aceite es: ", aceite)
+bol = agua  + harina + sal
+budare = bol + aceite
+print("Con la cantidad de ingredientes salieron", budare)
+input("Presione enter para salir")
